fde567d13e4ad463283b04061519cc62b577bbd8
Ajith Kumar <ajithk444@gmail.com>
2018-06-15 04:42:38 +0530

    Initial Commit

---
+++ First App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  First App
+//
+//  Created by Ajith Kumar on 15/06/18.
+//  Copyright © 2018 Ajith Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
