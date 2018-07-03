88c064bb5508cc0ec42b0a15d3da72bda21d3c43
Ajith Kumar <ajithk444@gmail.com>
2018-07-04 04:52:15 +0530

    Initial commit

--- First App/ViewController.swift
+++ First App/ViewController.swift
@@ -8,8 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum modes {
+    case not_set
+    case addition
+    case subtraction
+    case multiplication
+    case division
+}
 
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var labelOutput: UILabel!
+    
+    var labelString:String = "0"
+    var currentMode:modes = .not_set
+    var savedNum:Double = 0
+    var lastButtonWasMode:Bool = false
+    
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +36,93 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func didPressPlus(_ sender: Any) {
+        changeMode(newMode: .addition)
+    }
+    
+    @IBAction func didPressMinus(_ sender: Any) {
+        changeMode(newMode: .subtraction)
+    }
+    
+    @IBAction func didPressMultiply(_ sender: Any) {
+        changeMode(newMode: .multiplication)
+    }
+    
+    @IBAction func didPressDivide(_ sender: Any) {
+        changeMode(newMode: .division)
+    }
+    @IBAction func didPressEquals(_ sender: Any) {
+        guard  let labelInt:Double = Double(labelString) else {
+            return
+        }
+        if(currentMode == .not_set || lastButtonWasMode){
+            return
+        }
+        if(currentMode == .addition){
+            savedNum +=  labelInt
+        }
+        else if(currentMode == .subtraction){
+            savedNum -=  labelInt
+        }
+        else if(currentMode == .multiplication){
+            savedNum *=  labelInt
+        }
+        else if(currentMode == .division){
+            savedNum /=  labelInt
+        }
+        currentMode = .not_set
+        labelString = "\(savedNum)"
+        updateText()
+        lastButtonWasMode = true
+    }
+    
+    @IBAction func didPressClear(_ sender: Any) {
+        labelString = "0";
+        currentMode = .not_set
+        savedNum = 0
+        lastButtonWasMode = false
+        labelOutput?.text = "0"
+    }
+    
+   
+    @IBAction func didPressNumber(_ sender: UIButton) {
+        let stringValue:String? = sender.titleLabel?.text
+        
+        if(lastButtonWasMode){
+            lastButtonWasMode = false
+            labelString = "0"
+        }
+        
+        labelString = labelString.appending(stringValue!)
+        updateText()
+    }
+    
+    
+    func updateText(){
+        print("\(labelString)")
+        guard  let labelInt:Double = Double(labelString) else {
+            return
+        }
+        if(currentMode == .not_set)
+        {
+            savedNum = labelInt
+        }
+        
+        let numberFormatter:NumberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        let num:NSNumber = NSNumber(value: labelInt)
+        
+        labelOutput?.text = numberFormatter.string(from: num)
+        labelOutput.sizeToFit()
+    }
+    
+    func changeMode(newMode:modes){
+        if(savedNum==0){
+            return
+        }
+        currentMode = newMode
+        lastButtonWasMode = true
+    }
 }
 
